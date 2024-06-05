3af0e7fe9c78184afcacd20a5f4ca91b6bb678c2
gabbonj <61253079+gabbonj@users.noreply.github.com>
2024-06-05 13:19:11 +0200

    Window startup with genesis block creation

--- src/main/java/org/main/controllers/BlockchainView.java
+++ src/main/java/org/main/controllers/BlockchainView.java
@@ -1,4 +1,61 @@
 package org.main.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import org.blockchain.actors.Wallet;
+import org.blockchain.blockchain.Block;
+import org.blockchain.blockchain.Blockchain;
+import org.blockchain.events.Creation;
+import org.blockchain.events.Event;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SignatureException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class BlockchainView {
+    public Blockchain blockchain;
+
+    @FXML
+    private ListView<String> WalletsList;
+
+    private void createGenesis() throws NoSuchAlgorithmException, SignatureException, InvalidKeyException, NoSuchProviderException {
+        blockchain = new Blockchain();
+
+        Wallet w1 = new Wallet();
+        Creation c=  new Creation(w1);
+        ArrayList<Event> e = new ArrayList<>() {{
+            add(c);
+        }};
+        Block genesis = new Block(0, blockchain.getNext_zeros(), e, w1);
+        for (Event event : genesis.getEvents()) {
+            blockchain.addPending(event);
+        }
+        while (!genesis.verify()) {
+            genesis.setNonce(genesis.getNonce() + 1);
+        }
+        blockchain.addBlock(genesis);
+    }
+
+    private void updateWallets() {
+        WalletsList.setItems(FXCollections.observableArrayList(
+                blockchain.activeWallets().stream()
+                        .filter(Objects::nonNull)
+                        .map(w -> String.valueOf(w.getPublicKey().hashCode()))
+                        .collect(Collectors.toList())
+        ));
+    }
+
+    private void update() {
+        updateWallets();
+    }
+
+    public void initialize() throws NoSuchAlgorithmException, SignatureException, InvalidKeyException, NoSuchProviderException {
+        createGenesis();
+        update();
+    }
 }
