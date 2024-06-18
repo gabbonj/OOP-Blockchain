e6f348ee7451a62ec9d021a84ad7c4c69219cdf8
gabbonj <61253079+gabbonj@users.noreply.github.com>
2024-06-18 12:32:34 +0200

    Added logo

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -17,6 +18,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Parent view = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("blockchain-view.fxml")));
         Scene scene = new Scene(view);
+        stage.getIcons().add(new Image(String.valueOf(Objects.requireNonNull(getClass().getResource("logo.png")))));
         stage.setTitle("Blockchain");
         stage.setResizable(false);
         stage.setScene(scene);
